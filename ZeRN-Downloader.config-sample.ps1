9fe33673eb09c051bbb5f979bd93cc77916072f3
Michael <mn@michael-nolte.de>
2025-01-27 16:37:10 +0100

    Verzeichnisangabe für Attachments hinzugefügt

--- ZeRN-Downloader.config-sample.ps1
+++ ZeRN-Downloader.config-sample.ps1
@@ -4,18 +4,23 @@ $api_username = "erechnung@domain.de"
 # Passwort für den Zugriff auf die API
 $api_password = "VerYS3cretP@ssw0rd"
 
-# Verzeichnis in dem die Rechnungen im XML-Format gespeichert werden sollen
+# Verzeichnis, in dem die Rechnungen im XML-Format gespeichert werden sollen
 $files_xml = "$PSScriptRoot\xml"
 
-# Verzeichnis in dem die Validierungsreports der Rechnungen gespeichert werden sollen.
+# Verzeichnis, in dem die Validierungsreports der Rechnungen gespeichert werden sollen.
 # Leerer String deaktiviert das Herunterladen der Validierungsreports
 #$files_validation = "$PSScriptRoot\validation"
 $files_validation = ""
 
+# Verzeichnis, in dem die Anhänge der Rechnungen gespeichert werden sollen.
+# Leerer String deaktiviert das Herunterladen der Anhänge
+#$files_attachment = "$PSScriptRoot\attachment"
+$files_attachment = ""
+
 # Pfad, in dem die Protokolldateien gespeichern werden sollen
 $files_log = "$PSScriptRoot\log"
 
-# Detailgrad der Protokollieren. Mögliche Werte:
+# Detailgrad der Protokolle. Mögliche Werte:
 # - $log_debug
 # - $log_info (Standard)
 # - $log_warning
