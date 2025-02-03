4a616cbf680b3cecad9c21964f8b9ed3c2b8aff7
Michael <mn@michael-nolte.de>
2025-02-03 09:12:15 +0100

    Trennung XML-Syntax
    Trennung der Verzeichnisse nach XML-Syntax (CII/UBL) möglich

--- ZeRN-Downloader.config-sample.ps1
+++ ZeRN-Downloader.config-sample.ps1
@@ -4,8 +4,14 @@ $api_username = "erechnung@domain.de"
 # Passwort für den Zugriff auf die API
 $api_password = "VerYS3cretP@ssw0rd"
 
-# Verzeichnis, in dem die Rechnungen im XML-Format gespeichert werden sollen
+# Verzeichnis, in dem die Rechnungen im XML-Format gespeichert werden sollen, deren Syntax nicht erkannt wurde
 $files_xml = "$PSScriptRoot\xml"
+
+# Verzeichnis, in dem die Rechnungen im XML-Format mit CII-Syntax gespeichert werden sollen
+$files_xml_cii = "$PSScriptRoot\xml_cii"
+
+# Verzeichnis, in dem die Rechnungen im XML-Format mit UBL-Syntax gespeichert werden sollen
+$files_xml_ubl = "$PSScriptRoot\xml_ubl"
 
 # Verzeichnis, in dem die Validierungsreports der Rechnungen gespeichert werden sollen.
 # Leerer String deaktiviert das Herunterladen der Validierungsreports
@@ -20,7 +26,7 @@ $files_attachment = ""
 # Pfad, in dem die Protokolldateien gespeichern werden sollen
 $files_log = "$PSScriptRoot\log"
 
-# Detailgrad der Protokolle. Mögliche Werte:
+# Detailgrad der Protokollierung. Mögliche Werte:
 # - $log_debug
 # - $log_info (Standard)
 # - $log_warning
